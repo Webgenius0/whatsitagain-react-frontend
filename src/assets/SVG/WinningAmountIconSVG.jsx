67c8827f9dbd26f8f5c8250dfca7dce3c0ff6360
SomorMK <samormk6@gmail.com>
2025-10-09 10:55:44 +0600

    Create Match Form created without the selectboxes

---
+++ src/assets/SVG/WinningAmountIconSVG.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export default function WinningAmountIconSVG() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-full h-auto"
+      viewBox="0 0 22 22"
+      fill="none"
+    >
+      <path
+        d="M21 11C21 5.47715 16.5228 1 11 1C5.47715 1 1 5.47715 1 11C1 16.5228 5.47715 21 11 21C16.5228 21 21 16.5228 21 11Z"
+        stroke="#FFD700"
+        stroke-width="1.5"
+      />
+      <path
+        d="M11 1V3"
+        stroke="#FFD700"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M21 11L19 11"
+        stroke="#FFD700"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M11 19L11 21"
+        stroke="#FFD700"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M3 11H1"
+        stroke="#FFD700"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+      <path
+        d="M11 8C9.89543 8 9 8.67157 9 9.5C9 10.3284 9.89543 11 11 11C12.1046 11 13 11.6716 13 12.5C13 13.3284 12.1046 14 11 14M11 8C11.8708 8 12.6116 8.4174 12.8862 9M11 8V7M11 14C10.1292 14 9.38836 13.5826 9.1138 13M11 14V15"
+        stroke="#FFD700"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
